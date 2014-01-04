c5edfc5fa8649dc509795086d4c675932d75cfaf
mesquitadandao <mesquitadandao@users.noreply.github.com>
2014-01-04 13:09:03 -0300

    changeValTextTo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,17 @@
-console.log('This would be the main JS file.');
+;$(function(){
+	var $demoChangeValTextTo = $("#demo-changeValTextTo");
+	var $searchChangeValTextTo = $("#search-changeValTextTo");
+	var $replacementChangeValTextTo = $("#replacement-changeValTextTo");
+
+	var change = function(valText){
+		return valText.gsub($searchChangeValTextTo.val(), $replacementChangeValTextTo.val()).toUpperCase();
+	};
+
+	$("#apply-changeValTextTo-only-demo").on("click", function(){
+		$demoChangeValTextTo.md('changeValTextTo', change);
+	});
+
+	$("#apply-changeValTextTo-in-page").on("click", function(){
+		$('.change-element').md('changeValTextTo', change);
+	});
+});
